46907cf73983fdf0f78ce26f7042c329f213d8b4
joachim <joachimes@gmail.com>
2022-03-03 19:21:54 +0100

    able to render and clean up all obj files

--- blender/exploreObj.py
+++ blender/exploreObj.py
@@ -4,17 +4,18 @@ import glob
 import json
 
 
-data_dir = '/home/joachimes/dtu/graphicslab/data'
 
+data_dir = '/home/joachimes/dtu/graphicslab/data'
 dataset_name = 'ShapeNetCore.v2'
 json_name = 'shapenetcore.taxonomy.json'
+model_name = 'model_normalized.obj'
+target_classes = ['camera']
 
+bpy.context.scene.render.engine = 'BLENDER_EEVEE'
 bpy.context.scene.render.engine = 'CYCLES'
 bpy.context.scene.cycles.device = 'GPU'
-bpy.context.scene.render.engine = 'BLENDER_EEVEE'
 
 
-target_classes = ['camera']
 
 with open(os.path.join(data_dir, json_name)) as f:
     dataset_json = json.load(f)
@@ -27,50 +28,59 @@ for class_obj in dataset_json:
             class_paths += [class_obj['metadata']['name']]
         if not os.path.exists(os.path.join(data_dir, dataset_name, class_obj['metadata']['name'])):
             print(f'Data for class {class_obj["metadata"]["label"]} does not exist')
-print(class_paths)
-model_name = 'model_normalized.obj'
 
+
+# setup correct folder path
 if not os.path.exists(os.path.join(data_dir, 'renders')):
     os.mkdir(os.path.join(data_dir, 'renders'))
 else:
+    # Cleaning old renders
     for img in glob.glob(os.path.join(data_dir, 'renders', '*.png')):
         os.remove(img)
 
 for class_path in class_paths:
-    print(os.path.join(data_dir, dataset_name, class_path, '**', '*.obj'))
     
     model_files = glob.glob(os.path.join(data_dir, dataset_name, class_path, '**', '*.obj'), recursive=True)
-    print(model_files)
     i = 0
     for model_file in model_files:
+        
+        # Parse path
         model_file_split = model_file.split('/')
         model_hash_index = -2
         if model_file_split[-2] == 'models':
             model_hash_index = -3
+
+        # Import
+        bpy.ops.import_scene.obj(filepath=model_file)
+        
+        
         collection_name = model_file_split[model_hash_index]
         obj_name = model_file_split[-1].replace('.obj', '')
-        bpy.ops.import_scene.obj(filepath=model_file)
-        obj_object = bpy.context.selected_objects[0]
+        
+        
+        # Init collection
         myCol = bpy.data.collections.new(collection_name)
+        # Are these lines needed?
+        obj_object = bpy.context.selected_objects[0]
         bpy.context.scene.collection.children.link(myCol)
+        # add mesh to correct collection
         for ob in bpy.context.selected_objects:
             myCol.objects.link(ob)
             bpy.context.scene.collection.objects.unlink(ob)
+        
+        # Render pass
         bpy.context.scene.render.filepath = os.path.join(data_dir, 'renders', model_file_split[model_hash_index])
         bpy.ops.render.render(write_still = True)
 
+
+        # Remove Collection
         bpy.data.collections.remove(myCol)
 
-        for collec in bpy.data.collections:
-            if not collec.users:
-                print("no users")
+        # Delete all textures, materials and meshes
         for img in bpy.data.images:
-            if not img.users:
-                print("no users")
+            bpy.data.images.remove(img)
         for mesh in bpy.data.meshes:
-            if not mesh.users:
-                print("No users for mesh")
-        i += 1
+            bpy.data.meshes.remove(mesh)
+        for material in bpy.data.materials:
+            bpy.data.materials.remove(material)
 
-        if i == 2:
-            break
