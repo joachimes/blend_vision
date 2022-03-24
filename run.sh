bf0bf76ef3a625711371d95a1336a33aa7b3b5b7
Joachim Emanuel Svendsen <vfk540@alumni.ku.dk>
2022-03-24 12:53:36 +0100

    add multi platform start

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo $1
-# MacOS
-/Applications/Blender.app/Contents/MacOS/Blender --background --python blender/exploreObj.py
-
-# Ubuntu
-# /opt/Blender/Blend --background --python blender/exploreObj.py
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     /opt/Blender/Blend --background --python blender/exploreObj.py;;
+    Darwin*)    /Applications/Blender.app/Contents/MacOS/Blender --background --python blender/exploreObj.py;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
