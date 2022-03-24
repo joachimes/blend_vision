0139a95c5e1b3e1479bfbfec921c7508fbbaaa00
Joachim Emanuel Svendsen <vfk540@alumni.ku.dk>
2022-03-24 11:36:11 +0100

    Add run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo $1
+# MacOS
+/Applications/Blender.app/Contents/MacOS/Blender --background --python blender/exploreObj.py
+
+# Ubuntu
+# /opt/Blender/Blend --background --python blender/exploreObj.py
