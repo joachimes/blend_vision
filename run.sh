b4db5a07817af2c1476cb2dd800c1f7b9b30d8da
joachim <joachimes@gmail.com>
2022-04-20 20:54:36 +0200

    Fix path for ubuntu blender

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     /opt/Blender/Blend --background --python ./blender/exploreObj.py;;
+    Linux*)     /opt/Blender/blender --background --python ./blender/exploreObj.py;;
     Darwin*)    /Applications/Blender.app/Contents/MacOS/Blender --background --python ./blender/exploreObj.py;;
     CYGWIN*)    machine=Cygwin;;
     MINGW*)     machine=MinGw;;
